e3122afd0086d2aee8e2ecbb9ad0b9f89e93ab14
dmartinezsepulveda.external@epo.org <dmartinezsepulveda.external@epo.org>
2024-08-26 20:49:36 +0200

    updating cross origins

--- src/main/java/com/imagina/backend/controller/PostController.java
+++ src/main/java/com/imagina/backend/controller/PostController.java
@@ -3,39 +3,47 @@ package com.imagina.backend.controller;
 import com.imagina.backend.model.dtos.PostDto;
 import com.imagina.backend.service.PostService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
-@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
+    @Value("${app.cors.allowedOrigins}")
+    private String allowedOrigins;
+
+
+    @CrossOrigin(origins = "${app.cors.allowedOrigins}")
     @GetMapping
     public List<PostDto> getPosts() {
         return postService.getPosts();
     }
 
+    @CrossOrigin(origins = "${app.cors.allowedOrigins}")
     @GetMapping("/{id}")
     public PostDto getPostById(@PathVariable("id") Long id) {
         return postService.getPostById(id);
     }
 
+    @CrossOrigin(origins = "${app.cors.allowedOrigins}")
     @PostMapping
     public PostDto addPost(@RequestBody PostDto postDto) {
         return postService.addPost(postDto);
     }
 
+    @CrossOrigin(origins = "${app.cors.allowedOrigins}")
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable("id") Long id) {
         postService.deletePost(id);
     }
 
+    @CrossOrigin(origins = "${app.cors.allowedOrigins}")
     @PutMapping
     public PostDto updatePost(@RequestBody PostDto postDto) {
         return postService.updatePost(postDto);
